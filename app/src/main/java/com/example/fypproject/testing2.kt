436980656f2c42b74bd63ae0cbb87d643f41b1d3
jian hui <jianh04@gmail.com>
2022-11-24 11:15:07 +0800

    Mood Monitoring App

---
+++ app/src/main/java/com/example/fypproject/testing2.kt
@@ -0,0 +1,11 @@
+package com.example.fypproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class testing2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing2)
+    }
+}
